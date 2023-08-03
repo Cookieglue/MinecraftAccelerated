95b80b8734d617bd34a7333bca5033408ecd327f
mari <63924379+Cookieglue@users.noreply.github.com>
2023-08-03 10:07:43 -0400

    Added lang, drops, and fixed pumpkin blocks

--- src/main/java/mari/mcaccel/initializers/BlockInit.java
+++ src/main/java/mari/mcaccel/initializers/BlockInit.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 public class BlockInit {
 
-    private static final FabricBlockSettings BASIC_PUMPKIN_SETTINGS = FabricBlockSettings.copyOf(Blocks.PUMPKIN).requiresTool();
+    private static final FabricBlockSettings BASIC_PUMPKIN_SETTINGS = FabricBlockSettings.copyOf(Blocks.PUMPKIN);
     public static final Block CARVED_PUMPKIN_CHECK  = new WearableCarvedPumpkinBlock(BASIC_PUMPKIN_SETTINGS);
     public static final Block CARVED_PUMPKIN_CREEPER  = new WearableCarvedPumpkinBlock(BASIC_PUMPKIN_SETTINGS);
     public static final Block CARVED_PUMPKIN_DERP  = new WearableCarvedPumpkinBlock(BASIC_PUMPKIN_SETTINGS);
